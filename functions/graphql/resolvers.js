0baa33fcf61a73f0570ebe152954f9b7e3f2fd6b
Amaanuddin <amaanuddinparvez@gmail.com>
2021-04-15 19:19:04 +0530

    add and remove favorities methods added

--- functions/graphql/resolvers.js
+++ functions/graphql/resolvers.js
@@ -1,77 +1,94 @@
 const { default: axios } = require("axios");
-const { Get, Put } = require("../../common/db/dynamo");
+const { Get, Put, Update } = require("../../common/db/dynamo");
 
-  
-const bcrypt = require('bcryptjs');
-const jwt = require('jsonwebtoken');
+const bcrypt = require("bcryptjs");
+const jwt = require("jsonwebtoken");
+const { AuthenticationError } = require("apollo-server-errors");
 
-
-const MOVIE_BASE_URL = "https://api.themoviedb.org/3/"
+const MOVIE_BASE_URL = "https://api.themoviedb.org/3/";
 
 // Provide resolver functions for your schema fields
 exports.resolvers = {
   Query: {
     listMovies: async (parent, args, context, info) => {
-      const movies = await axios.get(`${MOVIE_BASE_URL}movie/popular?api_key=${process.env.MOVIES_API_KEY}&page=${args.pageNo}`);
-      console.log(movies)
-       return {
-        movies: movies.data.results,
-        page: movies.data.page,
-        total_pages: movies.data.total_pages,
-        total_results: movies.data.total_results
-      };
-    },
-    movie: async (parent, args, context, info) => {
-      const movie = await axios.get(`${MOVIE_BASE_URL}movie/${args.id}?api_key=${process.env.MOVIES_API_KEY}`);
-      console.log(movie)
-      return movie.data;
-    },
-    searchMovie: async (parent, args, context, info) => {
-      const movies = await axios.get(`${MOVIE_BASE_URL}search/movie?language=en&page=${args.pageNo}&query=${args.searchInput}&api_key=${process.env.MOVIES_API_KEY}`);
-      console.log(movies)
+      const movies = await axios.get(
+        `${MOVIE_BASE_URL}movie/popular?api_key=${process.env.MOVIES_API_KEY}&page=${args.pageNo}`
+      );
       return {
         movies: movies.data.results,
         page: movies.data.page,
         total_pages: movies.data.total_pages,
-        total_results: movies.data.total_results
+        total_results: movies.data.total_results,
+      };
+    },
+    movie: async (parent, args, context, info) => {
+      const movie = await axios.get(
+        `${MOVIE_BASE_URL}movie/${args.id}?api_key=${process.env.MOVIES_API_KEY}`
+      );
+      return movie.data;
+    },
+    searchMovie: async (parent, args, context, info) => {
+      const movies = await axios.get(
+        `${MOVIE_BASE_URL}search/movie?language=en&page=${args.pageNo}&query=${args.searchInput}&api_key=${process.env.MOVIES_API_KEY}`
+      );
+      return {
+        movies: movies.data.results,
+        page: movies.data.page,
+        total_pages: movies.data.total_pages,
+        total_results: movies.data.total_results,
       };
     },
     login: async (parent, args, context, info) => {
       const params = {
-            TableName : process.env.USER_TABLE_NAME,
-            Key: {
-                email: args.email
-            }
-        };
+        TableName: process.env.USER_TABLE_NAME,
+        Key: {
+          email: args.email,
+        },
+      };
       const { Item } = await Get(params);
       if (!Item) {
-        throw new Error('User not found.');
+        throw new Error("User not found.");
       }
       const isEqual = await bcrypt.compare(args.password, Item.password);
       if (!isEqual) {
-        throw new Error('Password is incorrect!');
+        throw new Error("Password is incorrect!");
       }
-        const token = jwt.sign(
-      { name: Item.name, email: Item.email },
-      'somesupersecretkey',
-      {
-        expiresIn: '1h'
+      const token = jwt.sign(
+        { name: Item.name, email: Item.email },
+        "somesupersecretkey",
+        {
+          expiresIn: "1h",
+        }
+      );
+      return { email: Item.email, token, tokenExpiration: 1 };
+    },
+    listFavoriteMovies: async (parent, args, context, info) => {
+      if (context.user.email !== args.email || !context.user) {
+        throw new AuthenticationError("Unauthorized!");
       }
-    );
-      return {email: Item.email, token, tokenExpiration: 1};
-    }
+      const movies = [];
+      await Promise.all(
+        Object.keys(args.fav_movies).map(async (id) => {
+          const movie = await axios.get(
+            `${MOVIE_BASE_URL}movie/${id}?api_key=${process.env.MOVIES_API_KEY}`
+          );
+          return movies.push({ ...movie.data, isFavorite: true });
+        })
+      );
+      return movies;
+    },
   },
   Mutation: {
     signUp: async (parent, args, context, info) => {
       const params = {
-            TableName : process.env.USER_TABLE_NAME,
-            Key: {
-                email: args.email
-            }
-        };
+        TableName: process.env.USER_TABLE_NAME,
+        Key: {
+          email: args.email,
+        },
+      };
       const { Item } = await Get(params);
       if (Item) {
-        throw new Error('User already exist');
+        throw new Error("User already exist");
       }
       const hashedPassword = await bcrypt.hash(args.password, 12);
       const putParams = {
@@ -79,20 +96,72 @@ exports.resolvers = {
         Item: {
           email: args.email,
           password: hashedPassword,
-          name: args.name
-        }
-      }
+          name: args.name,
+          fav_movies: {},
+        },
+      };
       try {
         const result = await Put(putParams);
-        if(result.$response.data)
-          return { email: args.email, password: '', fav_movies: [], name: args.name };
-        throw new Error('Something went wrong');
+        if (result.$response.data)
+          return {
+            email: args.email,
+            password: "",
+            fav_movies: {},
+            name: args.name,
+          };
+        throw new Error("Something went wrong");
+      } catch (err) {
+        return { email: "", password: "", fav_movies: {}, name: "" };
       }
-      catch (err) {
-        console.log(err);
-        return {email: '', password: '', fav_movies: [], name: ''}
+    },
+    addMovieToFavorities: async (parent, args, context, info) => {
+      if (context.user.email !== args.email || !context.user) {
+        throw new AuthenticationError("Unauthorized!");
       }
-    }
-  }
+      const params = {
+        TableName: process.env.USER_TABLE_NAME,
+        Key: {
+          email: args.email,
+        },
+        UpdateExpression: "SET fav_movies.#movieID = :movieID",
+        ExpressionAttributeNames: {
+          "#movieID": args.movieID,
+        },
+        ExpressionAttributeValues: {
+          ":movieID": args.movieID,
+        },
+        ReturnValues: "ALL_NEW",
+      };
+      const data = await Update(params);
+      return {
+        email: data.$response.data.Attributes.email,
+        fav_movies: data.$response.data.Attributes.fav_movies,
+        name: data.$response.data.Attributes.name,
+        password: "",
+      };
+    },
+    removeMovieFromFavorities: async (parent, args, context, info) => {
+      if (context.user.email !== args.email || !context.user) {
+        throw new AuthenticationError("Unauthorized!");
+      }
+      const params = {
+        TableName: process.env.USER_TABLE_NAME,
+        Key: {
+          email: args.email,
+        },
+        UpdateExpression: "Remove fav_movies.#movieID",
+        ExpressionAttributeNames: {
+          "#movieID": args.movieID,
+        },
+        ReturnValues: "ALL_NEW",
+      };
+      const data = await Update(params);
+      return {
+        email: data.$response.data.Attributes.email,
+        fav_movies: data.$response.data.Attributes.fav_movies,
+        name: data.$response.data.Attributes.name,
+        password: "",
+      };
+    },
+  },
 };
-
--- functions/graphql/schema.js
+++ functions/graphql/schema.js
@@ -1,46 +1,50 @@
-const { gql } = require('apollo-server-lambda');
-
+const { gql } = require("apollo-server-lambda");
 
 // Construct a schema, using GraphQL schema language
 
 exports.schema = gql`
+  scalar JSON
+
   type Query {
     listMovies(pageNo: Int = 1): PaginatedMovieResult
     movie(id: Int!): Movie
-    searchMovie(searchInput: String!, pageNo: Int = 1):PaginatedMovieResult
+    searchMovie(searchInput: String!, pageNo: Int = 1): PaginatedMovieResult
     login(email: String!, password: String!): AuthData
+    listFavoriteMovies(email: String!, fav_movies: JSON): [Movie]
   }
 
-   type Mutation {
+  type Mutation {
     signUp(email: String!, password: String!, name: String!): User!
+    addMovieToFavorities(email: String!, movieID: Int!): User!
+    removeMovieFromFavorities(email: String!, movieID: Int!): User
   }
-
 
   type Movie {
-      id: Int!
-      title: String!,
-      vote_average: Float,
-      overview: String!,
-      poster_path: String!
+    id: Int!
+    title: String!
+    vote_average: Float
+    overview: String!
+    poster_path: String!
+    isFavorite: Boolean
   }
 
   type PaginatedMovieResult {
-      movies: [Movie]
-      page: Int!
-      total_pages: Int!
-      total_results: Int!
+    movies: [Movie]
+    page: Int!
+    total_pages: Int!
+    total_results: Int!
   }
-  
+
   type User {
     email: String!
-    fav_movies: [Int]
+    fav_movies: JSON
     name: String!
     password: String!
   }
 
   type AuthData {
-  email: String!
-  token: String!
-  tokenExpiration: Int!
-}
+    email: String!
+    token: String!
+    tokenExpiration: Int!
+  }
 `;
