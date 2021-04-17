83094939e02a32048cba9ce0b1700ae7107102a2
Amaanuddin <amaanuddinparvez@gmail.com>
2021-04-17 15:59:57 +0530

    removed import for deleted  files

--- client/src/store/actions/index.js
+++ client/src/store/actions/index.js
@@ -1,8 +1,6 @@
-import userInfoActions from "./actionCreators/userInfo";
 import storeUserProfile from "./actionCreators/storeUserProfile";
 
 const Actions = {
-  userInfoActions,
   storeUserProfile,
 };
 export default Actions;
--- client/src/store/reducers/index.jsx
+++ client/src/store/reducers/index.jsx
@@ -1,10 +1,8 @@
 import { combineReducers } from 'redux';
 import user from './user';
-import userInfoReducer from './userInfo';
 
 const rootReducer = combineReducers({
   user,
-  userInfoReducer
 });
 
 export default rootReducer;
